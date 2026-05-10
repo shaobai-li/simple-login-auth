d8fd4b45b6393e09c4aacbe906cbd2394fcb313e
wyp512 <wyp1137667201@gmail.com>
2026-05-10 16:10:47 +0800

    feat(client): 新增登录页分栏布局并在 App 中挂载 Login

---
+++ client/src/Login.tsx
@@ -0,0 +1,14 @@
+export default function Login() {
+  return (
+    <div className="flex min-h-dvh w-full">
+      <div className="w-1/3 shrink-0 bg-white" />
+      <div className="relative min-h-dvh w-2/3 shrink-0">
+        <img
+          src="/log.png"
+          alt=""
+          className="absolute inset-0 size-full object-cover"
+        />
+      </div>
+    </div>
+  )
+}
